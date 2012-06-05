138dcf4d9d284d7867ada1f6461611a80af8dbc8
neil kodner <nkodner@gmail.com>
2012-06-05 09:49:41 -0400

    initial

---
+++ output_numbers_from_tweets.py
@@ -0,0 +1,40 @@
+#!/bin/python
+import re, json, sys
+debug = False
+def strip_items(str, start_pos, end_pos):
+  return str[0:start_pos]+str[end_pos:]
+
+for itm in sys.stdin:
+  line = itm.strip()
+  try:
+    data = json.loads(line)
+  except:
+    #print "couldn't convert %s to json" % line
+    continue
+  txt=data['text']
+  tostrip=[]
+  entities=data['entities']
+  for k,v in entities.iteritems():
+    for ent in v: 
+      try:
+        (start_pos,end_pos)=ent['indices']
+        tostrip.append((start_pos,end_pos))
+
+      except KeyError:
+        # no entities/indicies. pass
+        print "error hre"
+        pass
+
+  for x in sorted(tostrip, reverse=True):
+    txt = strip_items(txt, *x)
+  
+  if debug:
+    print 'original tweet: %s' % data['text']
+    print 'modified tweet: %s' % txt  
+  numbers = re.findall(r'\b\d+\b', txt)
+  for number in numbers:
+    number = long(number)
+    print number
+
+
+      
